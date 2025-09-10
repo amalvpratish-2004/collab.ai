760ea63552c09452ebecb2fce69ec6be346deecd
Amal VP <145320734+amalvpratish-2004@users.noreply.github.com>
2025-09-10 17:44:45 +0530

    meetings form

--- src/modules/meetings/server/procedures.ts
+++ src/modules/meetings/server/procedures.ts
@@ -7,8 +7,46 @@ import { createTRPCRouter, protectedProcedure } from "@/trpc/init";
 //import { TRPCError } from "@trpc/server";
 import { DEFAULT_PAGE, DEFAULT_PAGE_SIZE, MAX_PAGE_SIZE, MIN_PAGE_SIZE } from "@/constants";
 import { TRPCError } from "@trpc/server";
+import { meetingsInsertSchema, meetingsUpdateSchema } from "../schemas";
 
 export const meetingsRouter = createTRPCRouter({
+    create: protectedProcedure
+        .input(meetingsInsertSchema)
+        .mutation(async ({ input, ctx }) => {
+            //drizzle always returns array (since sql works like that)
+            const [ createdMeeting ] = await db
+                .insert(meetings)
+                .values({
+                    ...input,
+                    userId: ctx.auth.user.id,
+                })
+                .returning();
+    
+            return createdMeeting;
+        }),
+    update: protectedProcedure
+            .input(meetingsUpdateSchema)
+            .mutation(async ({ ctx, input }) => {
+                const [ updatedMeeting ] = await db  
+                    .update(meetings)
+                    .set(input)
+                    .where(
+                        and(
+                            eq(meetings.id, input.id),
+                            eq(meetings.userId, ctx.auth.user.id),
+                        )
+                    )
+                    .returning();
+    
+                if(!updatedMeeting){
+                    throw new TRPCError({
+                        code: "NOT_FOUND",
+                        message: "Agent not found"
+                    })
+                }
+    
+                return updatedMeeting;
+            }),
     getOne: protectedProcedure
         .input(z.object({ id: z.string() }))
         .query(async({ input, ctx }) => {
